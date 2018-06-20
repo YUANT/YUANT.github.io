09fd4e3b5f7ab21291299c7bf69060d59107cac2
小林　優 <yu_kobayashi@kbit.co.jp>
2018-06-20 17:35:52 +0900

    Workboxを使用してキャッシュを保持するように修正

--- service_worker/sw.js
+++ service_worker/sw.js
@@ -1,1 +1,21 @@
 // Service Worker用JS
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+  
+  workbox.routing.registerRoute(
+    '/service_worker/index.html',
+    workbox.strategies.staleWhileRevalidate()
+  );
+  
+  workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.staleWhileRevalidate()
+  );
+  
+  
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
